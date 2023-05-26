946ff8c734d5273c75e79ef66b79ce3034a32db7
mouss3abbb <mouss3abbb@gmail.com>
2023-05-26 22:29:42 +0300

    Finished main functions

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 import axios from "axios";
 import chalk from "chalk";
 import inquirer from "inquirer";
+import figlet from "figlet";
+import gradient from "gradient-string";
+import nanospinner, { createSpinner } from "nanospinner";
+import open from "open";
 const baseUrl = 'https://mirror.codeforces.com/api/'
-const problems = [];
+let problems = [];
 let handles = [];
+const problemset = [];
 const problemTags = [
     '2-sat',
     'binary search',
@@ -45,7 +50,45 @@ const problemTags = [
 ];
 let chosenTags = [];
 
-axios.get(`${baseUrl}problemset.problems`)
+const delay = async function (timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+};
+
+function welcome() {
+    figlet('Katy', (err, data) => {
+        console.log(gradient.pastel(data));
+    });
+}
+
+async function getUserProblems(user) {
+    try {
+        const response = await axios.get(baseUrl + 'user.status?handle=' + user);
+        for (const submission of response.data.result) {
+            if (submission.verdict == 'OK') {
+                problems = problems.filter(item => item.contestId != submission.problem.contestId);
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        console.log(chalk.bgRedBright('ERROR finding problems for specified users'));
+    }
+}
+
+async function readProblemSet() {
+    await delay(500);
+    const spinner = createSpinner('Reading problem set...').start();
+    try {
+        const response = await axios.get(baseUrl + 'problemset.problems');
+        for (const problem of response.data.result.problems) {
+            problems.push(problem.name);
+        }
+        spinner.success({ text: 'Problems are ready' });
+    } catch (error) {
+        console.log(chalk.bgRedBright('ERROR Fetching the problem set'));
+    }
+}
+
+axios.get(baseUrl + 'problemset.problems')
     .then((response) => {
         for (const problem of response.data.result.problems) {
             problems.push(problem);
@@ -61,11 +104,38 @@ async function readHandles() {
             type: 'input',
             name: 'handles',
             message: 'Enter handles separated by spaces',
+            default: 'tourist'
         }
     );
     handles = answers.handles.split(' ');
 }
-await readHandles();
+
+async function readRating() {
+    const min = await inquirer.prompt(
+        {
+            type: 'number',
+            name: 'min_rating',
+            message: 'Enter minimum rating',
+            default: 800,
+            validate: function (value) {
+                return value >= 800 && value <= 3500;
+            }
+        }
+    );
+    const max = await inquirer.prompt(
+        {
+            type: 'number',
+            name: 'max_rating',
+            message: 'Enter maximum rating',
+            default: min.min_rating + 400,
+            validate: function (value) {
+                return value >= 800 && value <= 3500;
+            }
+        }
+    );
+    return [Math.min(min.min_rating, max.max_rating),
+    Math.max(min.min_rating, max.max_rating)];
+}
 
 async function readTags() {
     const answers = await inquirer.prompt({
@@ -73,9 +143,52 @@ async function readTags() {
         name: 'tags',
         message: 'Choose preferred tags',
         choices: problemTags,
+        default: problemTags
     })
     chosenTags = answers.tags;
 }
+
+async function readNumberOfProblems() {
+    const answer = await inquirer.prompt({
+        type: 'number',
+        name: 'number',
+        message: 'Enter number of problems',
+        default: 5
+    })
+    return answer.number
+}
+
+welcome();
+await readProblemSet();
+await readHandles();
+const [min_rate, max_rate] = await readRating();
 await readTags();
+for (const handle of handles) {
+    await getUserProblems(handle);
+}
+let number_of_problems = await readNumberOfProblems();
+for (const problem of problems) {
+    for (const tag of chosenTags) {
+        if (problem.tags.includes(tag) && problem.rating >= min_rate && problem.rating <= max_rate && !problemset.some(element => element.contestId == problem.contestId && element.name == problem.name)) {
+            problemset.push(problem);
+            number_of_problems--;
+        }
+        if (number_of_problems <= 0) {
+            break;
+        }
+    }
+    if (number_of_problems <= 0) {
+        break;
+    }
+}
+
+function urlBuilder(problem_name) {
+    return 'https://www.codeforces.com/problemset/problem/' + problem_name.contestId + '/' + problem_name.index;
+}
 
 
+for (const problem of problemset) {
+    console.log(chalk.yellow(problem.name));
+    console.log(chalk.blue(urlBuilder(problem)));
+    open(urlBuilder(problem));
+}
